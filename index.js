c401b9af853c550b40e6c31caf5f9f3e29e2df58
chanwoo <chanwoo0607@naver.com>
2023-08-03 23:04:49 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 4005
+const config = require('./config/key')
+const {User} = require("./models/User");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://chanwoo000:jung980420!@chanwoo.sktmzr8.mongodb.net/',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology:true
 }).then(() => console.log('mongo connected'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! hi !!')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(()=>{
+        res.status(200).json({
+            success:true
+        })
+    }).catch((err)=>{
+        return res.json({success:false,err})
+    });
 })
 
 app.listen(port, () => {
